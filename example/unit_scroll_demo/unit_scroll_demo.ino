c8c8538cf0bdc2a85dd472f5974a5332b278b464
KOBAYASHI Jun <kobayashi@game-center.info>
2025-01-16 21:04:24 +0900

    Update unit_scroll_demo.ino
    Add: resetEncoder

--- example/unit_scroll_demo/unit_scroll_demo.ino
+++ example/unit_scroll_demo/unit_scroll_demo.ino
@@ -25,6 +25,7 @@ void setup()
     // scroll.begin(&Wire, SCROLL_ADDR, 2, 1, 400000U);
 
     Serial.printf("firmware version:%d, i2c address:0x%X\n", scroll.getFirmwareVersion(), scroll.getI2CAddress());
+    scroll.resetEncoder();
 }
 
 void loop()
